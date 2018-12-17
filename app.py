a29c0892b251c3f21a9c8f885e487a6eb11f76d8
Richard Ma <richard.ma.19850509@gmail.com>
2018-12-17 11:15:02 +0800

    render template

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
+import socket
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello, world'
+    info = {
+            'HostName': socket.gethostname(),
+            'IP': socket.gethostbyname(socket.gethostname()),
+    }
+    return render_template('index.html', data=info)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
